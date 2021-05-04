5b090378c9e94bd826087a93703b2bcacfa86fb1
minjunglee <3sally@naver.com>
2021-05-04 17:22:05 +0900

    ac: 프로그래머스 단어변환 / DFS.BFS

---
+++ DFS.BFS/프로그래머스_단어변환.js
@@ -0,0 +1,24 @@
+function solution(begin, target, words) {
+    let answer = 0; 
+    const q = []; 
+    const visit = Array(words.length); 
+    q.push([begin, answer]); 
+    console.log(`q=${q}, q.length=${q.length}`);
+    while (q.length) { 
+        let [s, cnt] = q.shift(); 
+        if (s == target) 
+            return cnt; 
+        words.forEach((w, i) => { // words의 단어들인 w와 begin의 문자를 비교하여 서로 다른 부분의 인덱스들을 구한다. 
+            const idx = [...w].reduce((a, c, i) => (c != s[i] ? a.push(i) : a, a), []); 
+            // words와 begin이 1개의 문자가 다르고 방문하지 않은 경우(변환된 문자가 아닌 경우) 
+            if (idx.length == 1 && !visit[i]) { visit[i] = 1; q.push([w, ++cnt]); } 
+        }) 
+    } 
+    return answer;
+}
+
+var begin = "hit";
+var target = "cog";
+var words = ["hot", "dot", "dog", "lot", "log", "cog"]	;
+
+console.log(solution(begin,target,words));
