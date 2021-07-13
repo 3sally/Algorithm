f2194655082015d9d3936d7fd32f6089f8b952b1
minjunglee <3sally@naver.com>
2021-07-13 23:26:37 +0900

    ac: 백준 일곱난쟁이 / 배열

---
+++ 배열/백준_2309_일곱난쟁이.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+
+public class BackJoon_2309_일곱난쟁이{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> all = new ArrayList<Integer>();
+		int sum = 0;
+		
+		for(int i=0;i<9;i++) {
+			all.add(sc.nextInt());
+			sum += all.get(i);
+		}
+		
+		int minusHeight = sum-100;
+		
+		for(int i=0; i<all.size(); i++) {
+			for(int j=i+1; j<all.size(); j++) {
+				if(all.get(i)+all.get(j)==minusHeight) {
+					all.remove(i);
+					all.remove(j-1);
+					break;
+				}
+			}
+		}
+		
+		Collections.sort(all);
+		
+		for(int i:all) {
+			System.out.println(i);
+		}
+	}
+}
