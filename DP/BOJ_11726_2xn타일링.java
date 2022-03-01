1fed08e098bf5af81a401753c02e26fd9f7df315
minjunglee <3sally@naver.com>
2022-03-01 21:47:27 +0900

    ac: BOJ 2xn타일링 / DP

---
+++ DP/BOJ_11726_2xn타일링.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class BOJ_11726_2xn타일링 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		long[] dp = new long[1001];
+
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= N; i++) {
+			dp[i] = dp[i - 2] + dp[i - 1];
+			dp[i] %= 10007;
+		}
+
+		System.out.println(dp[N]);
+	}
+}
