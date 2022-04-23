3f162edefd860ecf8442559f57298600b9effaa6
minjunglee <3sally@naver.com>
2022-04-24 02:09:41 +0900

    ac: BOJ 이전순열 / 순열

---
+++ 순열/BOJ_10973_이전순열.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10973_이전순열 {
+    static int N, arr[];
+    static String str;
+    static boolean visited[];
+    static boolean check;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int[] input = new int[N];
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (np(input)) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++) {
+                sb.append(input[i]).append(" ");
+            }
+            System.out.println(sb);
+        } else {
+            System.out.println("-1");
+        }
+
+    }
+
+    private static boolean np(int[] input) {
+        int i = input.length - 1;
+        while (i > 0 && input[i - 1] <= input[i])
+            --i;
+        if (i <= 0)
+            return false;
+
+        int j = input.length - 1;
+        while (input[j] >= input[i-1])
+            --j;
+
+        swap(input, i-1, j);
+
+        int k = input.length - 1; // 맨뒤부터
+        while (i < k) {
+            swap(input, i++, k--);
+        }
+
+        return true;
+    }
+
+    private static void swap(int[] input, int i, int j) {
+        int temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+    }
+}
