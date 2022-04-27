5a197059e85d87e5e7ca52b22c54fa07a8e62834
minjunglee <3sally@naver.com>
2022-04-28 00:53:22 +0900

    ac: BOJ N과M11 / 순열

---
+++ 순열/BOJ_15665_N과M11.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class BOJ_15665_N과M11 {
+    static int N, M, num[];
+    static LinkedHashSet<String> numList;
+    static int[] selected;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        num = new int[N];
+        st= new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+
+        selected = new int[M];
+        numList = new LinkedHashSet<>();
+
+        perm(0);
+        StringBuilder sb = new StringBuilder();
+        for (String list : numList) {
+            sb.append(list);
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void perm(int cnt) {
+        if(cnt==M){
+            String str = "";
+            for (int i = 0; i < M; i++) {
+                str += selected[i] + " ";
+            }
+            numList.add(str);
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            selected[cnt] = num[i];
+            perm(cnt+1);
+        }
+    }
+}
