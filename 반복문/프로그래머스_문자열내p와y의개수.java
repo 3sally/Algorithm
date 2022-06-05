7beb07db9261fde5915337d745cc77e5df6f8f24
minjunglee <3sally@naver.com>
2022-06-05 17:26:19 +0900

    ac: 프로그래머스 문자열내p와y의개수 / 반복문

---
+++ 반복문/프로그래머스_문자열내p와y의개수.java
@@ -0,0 +1,21 @@
+class Solution {
+    boolean solution(String s) {
+        s = s.toLowerCase();
+        int pCnt = 0; 
+        int yCnt = 0; 
+        
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i)=='p'){
+                pCnt++;
+            }else if(s.charAt(i)=='y'){
+                yCnt++;
+            }
+        }
+        
+        if(pCnt==yCnt){
+            return true;
+        }else{
+            return false;   
+        }
+    }
+}
