d59a9e39c7ecf90a0e6bc7c773c459595f0905c7
minjunglee <3sally@naver.com>
2022-06-08 17:56:26 +0900

    ac: 프로그래머스 부족한금액계산하기 / 반복문

---
+++ 반복문/프로그래머스_부족한금액계산하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        
+        long totalPrice = 0;
+        
+        for(int i = 1; i<=count; i++){
+            totalPrice += (price*i);
+        }
+        
+        
+        if(totalPrice<=money)
+            return 0;
+        
+        return totalPrice - money;
+    }
+}
